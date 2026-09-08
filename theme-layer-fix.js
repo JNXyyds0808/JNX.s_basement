3943651049879926836367145ffaaa9772a1eaf9
JNX <jiangnuoxi60@gmail.com>
2026-09-08 07:25:12 -0400

    Bust changelog cache for latest entries

--- theme-layer-fix.js
+++ theme-layer-fix.js
@@ -98,7 +98,7 @@
   function init(){
     forceTabletTitle();
     promote();
-    loadScriptOnce('site-changelog.js?v=1','data-jnx-changelog-loader');
+    loadScriptOnce('site-changelog.js?v=2','data-jnx-changelog-loader');
     loadScriptOnce('profile-enhancement.js?v=2','data-jnx-profile-enhancement-loader');
     loadScriptOnce('points-system.js?v=1','data-jnx-points-loader');
     loadScriptOnce('points-shop.js?v=1','data-jnx-points-shop-loader');
